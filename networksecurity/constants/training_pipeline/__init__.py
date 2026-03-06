b5d9c767e7ef51a0c94134706f8f13f1e40d3b64
Jainish2107 <jainish2107@gmail.com>
2026-03-06 17:41:38 +0530

    Data Ingestion Config Done

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import sys
+
+TARGET_COLUMN = "result"
+PIPELINE_NAME = "networksecurity"
+ARTIFACT_NAME = "artifiacts"
+FILE_NAME = "phisingData.csv"
+
+TRAIN_FILE = "train.csv"
+TEST_FILE = "test.csv"
+
+
+DATA_INGESTION_DATABASE_NAME = "NetworkSecurity"
+DATA_INGESTION_COLLECTION_NAME = "dataset"
+DATA_INGESTION_DIR_NAME = "data_ingestion"
+DATA_INGESTION_INGESTED_DIR = "ingested"
+DATA_INGESTION_FEATURE_STORE_DIR = "feature_store"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION = 0.2
